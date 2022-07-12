d849d8ba4b984a571e945b9ad08af7b5d22e1cd1
Luke Billings <lukecbillings@gmail.com>
2022-07-12 16:58:32 +0100

    refactored gif list

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -3,10 +3,17 @@ import Gif from './gif.jsx';
 import App from './app.jsx';
 
 class GifList extends Component {
+  renderList = () => {
+      return this.props.gifs.map(gif => {
+        return <Gif id={gif.id} key={gif.id} />
+      })
+  }
   render() {
-    return this.props.gifs.map(gif => {
-      return <Gif id={gif.id} key={gif.id} />
-    });
+    return (
+    <div className="gif-list">
+      {this.renderList()}
+    </div>
+    );
   }
 }
 
