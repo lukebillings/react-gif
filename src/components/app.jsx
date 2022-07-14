a3f2646d90331187147b626ca1733af538e2f2fe
Luke Billings <lukecbillings@gmail.com>
2022-07-14 22:26:28 +0100

    gif list changing on search

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,20 +2,49 @@ import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // array of gifs props will change
+      gifs: [],
+      // the id of the selected gif will change from null to an id, then to another id
+      selectedGifId: "DFexVkRG7gX9oCy68r"
+    }
+
+    this.search("homer thinking");
+  }
+
+  search = (query) => {
+    giphy('hAdPLvZK55nDjzGFQM4GiH0OMC5PTczQ').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+  }, (err, res) => {
+      // Res contains gif data!
+      console.log(res);
+      this.setState({
+        gifs: res.data
+      });
+  });
+ }
+
+
   render() {
     const gifs = [{ id: "DFexVkRG7gX9oCy68r" }, { id: "DFexVkRG7gX9oCy68q"}]
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-           <Gif id="DFexVkRG7gX9oCy68r"/>
+           <Gif id={this.state.selectedGifId}/>
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
       );
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  handleUpdate = (event) => {
+    console.log(event.target.value);
+    this.props.searchFunction(event.target.value);
+  }
   render() {
     return (
-      <input type="text" className="form-search form-control" />
+      <input type="text" className="form-search form-control" onChange={this.handleUpdate}/>
       );
   }
 }
